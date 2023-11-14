4e533d6ae085758f788e9a95458599dbee5791a1
Anubhav Garg <garg.anubhav.xyz@gmail.com>
2023-11-13 22:49:06 -0600

    Split into two different functions

--- api_combos.py
+++ api_combos.py
@@ -1,37 +1,43 @@
 import requests
 import json
 
+
 zip = 94111
 ZipString = str(zip)
 
 
-# weatherUrl = "https://us-weather-by-zip-code.p.rapidapi.com/getweatherzipcode"
+# function will return some weather data for a given zip code
+def getWeatherOutput (ZipString):
+	weatherUrl = "https://us-weather-by-zip-code.p.rapidapi.com/getweatherzipcode"
 
-# querystring = {"zip": ZipString}
+	querystring = {"zip": ZipString}
 
-# headers = {
-# 	"X-RapidAPI-Key": "6795ba421bmsh8f363c73274493bp14dbb7jsn03a28699e5d0",
-# 	"X-RapidAPI-Host": "us-weather-by-zip-code.p.rapidapi.com"
-# }
+	headers = {
+		"X-RapidAPI-Key": "6795ba421bmsh8f363c73274493bp14dbb7jsn03a28699e5d0",
+		"X-RapidAPI-Host": "us-weather-by-zip-code.p.rapidapi.com"
+	}
 
-# response = requests.get(weatherUrl, headers=headers, params=querystring)
+	response = requests.get(weatherUrl, headers=headers, params=querystring)
 
-# print(response.json())
+	print(response.json())
 
-# print(response.json()['City'])
+	print(response.json()['City'])
+	
 
-url = "https://api.rentcast.io/v1/markets?"
+# function will return some housing data for a given zip code
+def getHousingOutput(ZipString) : 
+	url = "https://api.rentcast.io/v1/markets?"
 
-headers = {
-    "accept": "application/json",
-    "X-Api-Key": "7bb22ecba95544d4bab9191475a5bd92"
-}
+	headers = {
+		"accept": "application/json",
+		"X-Api-Key": "7bb22ecba95544d4bab9191475a5bd92"
+	}
 
-housingParams = {
-    "zipCode" : zip, 
-    "historyRange" : '6'
-}
+	housingParams = {
+		"zipCode" : ZipString, 
+		"historyRange" : '6'
+	}
 
-response = requests.get(url, headers=headers, params = housingParams)
+	response = requests.get(url, headers=headers, params = housingParams)
 
-print (response.json())
+	print (response.json())
